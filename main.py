91347e5fab1b6a9c94aa6af8cf7469c4079435a6
Dan Laptop <dbroughton_09@hotmail.com>
2020-08-09 17:45:20 +0100

    add /user data endpoint - returns json

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
 import os
 import settings
-from flask import Flask
+
+from flask import Flask, jsonify
+
 app = Flask(__name__)
 
 @app.route('/')
 def root():
     return 'Returning some data...'
+
+
+@app.route('/users', methods=['GET'])
+def users():
+    return jsonify({
+        'users': [
+            {
+                'id': 'ajf924jgdfvn0e09',
+                'username': 'dan'
+            }
+        ]
+    })
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
